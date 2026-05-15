9c9f0e92afd448dabd633198a1bf87810385ae2d
iamkira420 <lmboniswa01@gmail.com>
2026-05-15 02:08:28 +0200

    Entry point for app - creates the main window

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
-using namespace std;
+/**
+ * @file main.cpp
+ * @brief Entry point for the Journal Article Database application.
+ * @author BK201
+ *
+ * Initialises the Qt application, creates and shows the main window.
+ */
+
+#include <QApplication>
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    // Set a clean application-wide style
+    app.setStyle("Fusion");
+
+    MainWindow w;
+    w.setWindowTitle("Journal Article Database");
+    w.resize(1200, 650);
+    w.show();
+
+    return app.exec();
+}
